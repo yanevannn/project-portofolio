618d74e4e64b5370ab72ce3ca374cf019ac744a8
yanevannn <yanevan03@gmail.com>
2025-02-16 14:51:04 +0800

    feat: About section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import About from './components/About.vue';
 import Hero from './components/Hero.vue'
 </script>
 
 <template>
   <Hero/>
+  <About/>
 </template>
 
 <style scoped>
---
+++ src/components/About.vue
@@ -0,0 +1,46 @@
+<script setup>
+</script>
+
+<template>
+    <div class="section px-6 h-screen flex items-center justify-center" id="about">
+        <div class="container mx-auto max-w-7xl grid grid-cols-1 lg:grid-cols-2 gap-10">
+            <div class="left flex items-center justify-center">
+                <div class="image relative">
+                    <img src="https://cdn.prod.website-files.com/606a802fcaa89bc357508cad/6070b0658f4803368fde5a9e_freelance-web-developer-at-work.jpeg"
+                        class="w-full h-auto rounded-xl" />
+                </div>
+            </div>
+
+            <div class="right flex flex-col justify-center lg:pl-10">
+                <div class="title">
+                    <span class="text-xl font-semibold uppercase font-poppins text-gray-600">About Me</span>
+                    <h3 class="text-3xl font-bold font-poppins text-gray-900 leading-tight">
+                        Web Developer: Eager to Learn & Contribute
+                    </h3>
+                </div>
+                <div class="text mt-4">
+                    <p class="text-base font-mulish text-gray-700 leading-relaxed text-justify">
+                        I am a recent graduate from ITB STIKOM Bali with a degree in
+                        Information Systems. Passionate about web development, I specialize
+                        in building and managing scalable applications. My expertise
+                        includes Laravel for backend development, Tailwind CSS for modern UI
+                        styling, and Nginx for server configuration. I also have experience
+                        managing cloud infrastructure using Google Cloud Platform, handling
+                        databases with MySQL, and working with Node.js for backend
+                        services. Additionally, I use Git for version control and VS Code as
+                        my preferred code editor. I am always eager to learn new technologies
+                        and contribute to innovative projects.
+                    </p>
+                </div>
+                <div class="button mt-6">
+                    <a href="#"
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-poppins font-bold py-3 px-6 rounded-full transition duration-300 ">
+                        Download CV
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
